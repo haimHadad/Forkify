86014196bd712c5f9fa1d54ace529228103bb369
משפחת חדד <haim.hddd@gmail.com>
2020-10-03 21:48:58 +0300

    Render recipes to DOM

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search'
 import * as searchView from './views/searchView'
+import * as recipeView from './views/recipeView'
 import {elements,renderLoader,clearLoader} from './views/base'
 import Recipe from './models/Recipe';
 /*Global state of the app
@@ -66,17 +67,21 @@ elements.searchResPages.addEventListener('click', e=>{
     const id = window.location.hash.replace('#','');
     if(id){
         //Prepare UI for changes
-
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
         //Create new Recipe object
         state.recipe = new Recipe(id);
-        //Get recipe data
+        
         try{
+            //Get recipe data and parse ingredients
             await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
             //Calculate servings and time
             state.recipe.calcTime();
             state.recipe.calcServings();
             //Render recipe
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         }catch(error){
             alert('Error processing with recipe !!');
         }
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -31,4 +31,64 @@ export default class Recipe{
     calcServings(){
         this.servings = 4;
     }
+
+    parseIngredients(){
+        const unitsLong = ['tablespoons','tablespoon', 'ounces','ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
+        const unitsShort =['tbsp','tbsp', 'oz', 'oz', 'tsp','tsp', 'cup', 'pounds'];
+        const units =[...unitsShort, 'kg', 'gr'];
+        const newIngredients = this.ingredients.map(el =>{
+           // 1) Uniform units
+           let ingredient = el.toLowerCase();
+           unitsLong.forEach((unit,i) => {
+            ingredient = ingredient.replace( unit , unitsShort[i] );
+           });
+
+           // 2) Remove parentheses
+           ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
+
+           // 3) Parse ingredients into count, unit and ingredients
+           const arrIng = ingredient.split(' ');
+           
+           const unitIndex = arrIng.findIndex(el2 =>units.includes(el2));
+            let objIng;
+            
+
+            if(unitIndex > -1){
+                //There is a unit
+                //Exchange 4 1/2 cups to arrCount [4,1/2] --> eval("4+1/2") --> 4.5
+                const arrCount = arrIng.slice(0,unitIndex);
+                let count;
+                if(arrCount.length === 1){
+                    count = eval(arrCount[0].replace('-', '+'));
+                }else{
+                    count = eval(arrCount.slice(0, unitIndex).join('+'));
+                }
+
+                objIng ={
+                    count,
+                    unit: arrIng[unitIndex],
+                    ingredient: arrIng.slice(unitIndex+1).join(' ')
+                }
+
+            }else if(parseInt(arrIng[0],10)){
+                //There is NO unit, but 1st element is a number
+                objIng ={
+                    count: parseInt(arrIng[0],10),
+                    unit: '',
+                    ingredient: arrIng.slice(1).join(' ')
+                }
+            }else if(unitIndex === -1){
+                //There is NO unit and No number in 1st position
+                objIng ={
+                    count:1,
+                    unit: '',
+                    ingredient
+                }
+            }
+
+
+           return objIng;
+        });
+        this.ingredients = newIngredients;
+    }
 } 
